8e556a1bc71edff41d3b8e204604727838975fd6
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2023-07-04 20:28:06 +0700

    th

--- src/main/java/niit/com/vn/springboot08/controller/HomeController.java
+++ src/main/java/niit/com/vn/springboot08/controller/HomeController.java
@@ -1,17 +1,34 @@
 package niit.com.vn.springboot08.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller("homeController")
 public class HomeController {
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("name", "Nguyen Van A");
+        model.addAttribute("age", 20);
         return "index";
     }
 
     @GetMapping("/about")
     public String about() {
-        return "about";
+        return "test/about";
+    }
+
+    @PostMapping("/do-login")
+    public String doLogin() {
+        return "index";
+    }
+    // @GetMapping
+    @RequestMapping(path = "/login",
+            method = { RequestMethod.GET, RequestMethod.POST })
+    public String login() {
+        return "login";
     }
 }
