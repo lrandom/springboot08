6932b046c45df0dcce540f93928e77173ee1d3e1
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2023-07-04 19:52:05 +0700

    init

---
+++ src/test/java/niit/com/vn/springboot08/Springboot08ApplicationTests.java
@@ -0,0 +1,13 @@
+package niit.com.vn.springboot08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
