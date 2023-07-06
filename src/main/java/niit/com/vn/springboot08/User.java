76aa46e4bc3cf441e546d3859c55e571d7cc3a56
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2023-07-06 20:03:02 +0700

    demo form binding

--- src/main/java/niit/com/vn/springboot08/User.java
+++ src/main/java/niit/com/vn/springboot08/User.java
@@ -1,6 +1,34 @@
 package niit.com.vn.springboot08;
 
+import lombok.Data;
+
+
 public class User {
     public String name;
+    public String address;
     public int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
