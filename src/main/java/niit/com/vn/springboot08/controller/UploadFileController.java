b724359fda1cf768b4f451193489d149b3b21b9f
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2023-07-20 20:55:54 +0700

    update demo

--- src/main/java/niit/com/vn/springboot08/controller/UploadFileController.java
+++ src/main/java/niit/com/vn/springboot08/controller/UploadFileController.java
@@ -1,16 +1,17 @@
 package niit.com.vn.springboot08.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.nio.file.Path;
 import java.util.Calendar;
 
 @Controller("uploadFileController")
@@ -38,7 +39,7 @@ public class UploadFileController {
                 //tạo thư mục mới
                 folder.mkdir();
             }
-            FileOutputStream fileOutputStream = new FileOutputStream(uploadDir + newDir + "/" + System.currentTimeMillis()+file.getOriginalFilename());
+            FileOutputStream fileOutputStream = new FileOutputStream(uploadDir + newDir + File.separator + System.currentTimeMillis() + file.getOriginalFilename());
             fileOutputStream.write(file.getBytes());
         } catch (Exception e) {
             e.printStackTrace();
@@ -46,8 +47,29 @@ public class UploadFileController {
         return "redirect:upload-file";
     }
 
-/*    @GetMapping("/file")
-    public String serveFile() {
-        BufferedReader bufferedReader = new BufferedReader();
-    }*/
+    @GetMapping("/file")
+    public ResponseEntity<byte[]> serveFile(@RequestParam("file-name") String fileName) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(uploadDir + fileName);
+            byte[] bytes = new byte[fileInputStream.available()];
+            fileInputStream.read(bytes);
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @GetMapping("/read-file/{fileName}")
+    public ResponseEntity<byte[]> readFile(@PathVariable String fileName) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(uploadDir + "7_2023/"+fileName);
+            byte[] bytes = new byte[fileInputStream.available()];
+            fileInputStream.read(bytes);
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
