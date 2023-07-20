df0dba6bd54c0f5cb25ba63fd3ba87346ce8e1e1
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2023-07-20 19:27:55 +0700

    session-3

--- src/main/java/niit/com/vn/springboot08/controller/UploadFileController.java
+++ src/main/java/niit/com/vn/springboot08/controller/UploadFileController.java
@@ -1,5 +1,6 @@
 package niit.com.vn.springboot08.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,10 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Calendar;
 
 @Controller("uploadFileController")
 public class UploadFileController {
+    @Value("${UPLOAD_DIR}")
+    public String uploadDir;
+
     @GetMapping("/upload-file")
     public String uploadFile() {
         return "upload-file";
@@ -19,7 +25,19 @@ public class UploadFileController {
     @PostMapping("/upload-file-handler")
     public String uploadFileHandler(@RequestParam("img") MultipartFile file) {
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream("/Users/lrandom/Documents/uploads/"+file.getOriginalFilename());
+            //get current month
+            //get current year
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);//lấy về năm
+            int month = calendar.get(Calendar.MONTH) + 1;//lấy về tháng
+            String newDir = month + "_" + year; //7_2023
+            //check xem thư mục mới này đã có trong đường dẫn upload chưa
+            File folder = new File(uploadDir + newDir);
+            if (!folder.exists() || folder.isFile()) {
+                //tạo thư mục mới
+                folder.mkdir();
+            }
+            FileOutputStream fileOutputStream = new FileOutputStream(uploadDir + newDir + "/" + System.currentTimeMillis()+file.getOriginalFilename());
             fileOutputStream.write(file.getBytes());
         } catch (Exception e) {
             e.printStackTrace();
