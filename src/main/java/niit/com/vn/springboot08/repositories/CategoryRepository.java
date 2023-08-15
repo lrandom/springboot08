fe5e678ca9c80461935be3b0e1b363b135464f33
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2023-08-15 20:18:35 +0700

    update

--- src/main/java/niit/com/vn/springboot08/repositories/CategoryRepository.java
+++ src/main/java/niit/com/vn/springboot08/repositories/CategoryRepository.java
@@ -3,15 +3,16 @@ package niit.com.vn.springboot08.repositories;
 import niit.com.vn.springboot08.entities.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CategoryRepository
-        extends CrudRepository<Category,Long>,
-        PagingAndSortingRepository<Category, Long> {
-
+public interface CategoryRepository extends CrudRepository<Category, Long>, PagingAndSortingRepository<Category, Long> {
+    @Query("SELECT u from categories u")
+    List<Category> myFindAll();
 }
